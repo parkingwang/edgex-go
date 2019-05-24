d93c6a4c9484783188d04e79a09696f326d2e18e
yoojia.chen <yoojiachen@gmail.com>
2019-05-24 14:12:46 +0800

    Update inspect struct

--- inspect.go
+++ inspect.go
@@ -4,20 +4,26 @@ package edgex
 // Author: 陈哈哈 chenyongjia@parkingwang.com, yoojiachen@gmail.com
 //
 
+const (
+	DeviceTypeEndpoint = "ENDPOINT"
+	DeviceTypeTrigger  = "TRIGGER"
+)
+
 // Inspect消息是将节点相关信息的描述
 type Inspect struct {
-	OS      string   `json:"os,omitempty"`
-	Arch    string   `json:"arch,omitempty"`
-	Devices []Device `json:"devices,omitempty"`
+	HostOS     string   `json:"hostOs,omitempty"`
+	HostArch   string   `json:"hostArch,omitempty"`
+	Vendor     string   `json:"vendor,omitempty"`     // 品牌名称
+	DriverName string   `json:"driverName,omitempty"` // 服务驱动类型名
+	Devices    []Device `json:"devices,omitempty"`    // 设备列表
 }
 
 type Device struct {
-	// DeviceName
-	Name string `json:"name,omitempty"`
-	// Is virtual
-	Virtual bool `json:"virtual,omitempty"`
-	// Description of device
-	Desc string `json:"desc,omitempty"`
-	// Exec command
-	Command string `json:"command,omitempty"`
+	Name       string `json:"name,omitempty"`       // 设备名称
+	Desc       string `json:"desc,omitempty"`       // 设备描述信息
+	Type       string `json:"type,omitempty"`       // 设备类型
+	Virtual    bool   `json:"virtual,omitempty"`    // 是否为虚拟设备，即通过代理后转换的设备
+	Command    string `json:"command,omitempty"`    // 设备控制命令
+	Address    string `json:"address,omitempty"`    // 控制通讯地址
+	EventTopic string `json:"eventTopic,omitempty"` // 发送事件Topic
 }
