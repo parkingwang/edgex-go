26d5e70e05d8926008a8af6b106f8abaaa12b9b3
YooJia.Chen <yoojiachen@gmail.com>
2019-05-26 12:49:08 +0800

    Add attrs

--- inspect.go
+++ inspect.go
@@ -11,19 +11,20 @@ const (
 
 // Inspect消息是将节点相关信息的描述
 type Inspect struct {
-	HostOS     string   `json:"hostOs,omitempty"`
-	HostArch   string   `json:"hostArch,omitempty"`
-	Vendor     string   `json:"vendor,omitempty"`     // 品牌名称
-	DriverName string   `json:"driverName,omitempty"` // 服务驱动类型名
-	Devices    []Device `json:"devices,omitempty"`    // 设备列表
+	HostOS     string   `json:"hostOS"`
+	HostArch   string   `json:"hostArch"`
+	Vendor     string   `json:"vendor"`     // 品牌名称
+	DriverName string   `json:"driverName"` // 服务驱动类型名
+	Devices    []Device `json:"devices"`    // 设备列表
 }
 
 type Device struct {
-	Name       string `json:"name,omitempty"`       // 设备名称
-	Desc       string `json:"desc,omitempty"`       // 设备描述信息
-	Type       string `json:"type,omitempty"`       // 设备类型
-	Virtual    bool   `json:"virtual,omitempty"`    // 是否为虚拟设备，即通过代理后转换的设备
-	Command    string `json:"command,omitempty"`    // 设备控制命令
-	Address    string `json:"address,omitempty"`    // 控制通讯地址
-	EventTopic string `json:"eventTopic,omitempty"` // 发送事件Topic
+	Name       string                 `json:"name"`       // 设备名称
+	Desc       string                 `json:"desc"`       // 设备描述信息
+	Type       string                 `json:"type"`       // 设备类型
+	Virtual    bool                   `json:"virtual"`    // 是否为虚拟设备，即通过代理后转换的设备
+	Command    string                 `json:"command"`    // 设备控制命令
+	Address    string                 `json:"address"`    // 控制通讯地址
+	EventTopic string                 `json:"eventTopic"` // 发送事件Topic
+	Attrs      map[string]interface{} `json:"attrs"`
 }
